e2f3841937f66944efd95ff3230e3fa7bedf1289
till90 <till.mueller1990@gmail.com>
2026-01-10 22:03:28 +0100

    remove unenessecary imports

--- main.py
+++ main.py
@@ -1195,4 +1195,4 @@ def index() -> Response:
 if __name__ == "__main__":
     # Local dev only (Cloud Run uses gunicorn)
     port = int(os.environ.get("PORT", "8080"))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
